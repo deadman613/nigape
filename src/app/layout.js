ba7555b3ac025017e05566b63a09a7a2c3039437
deadman613 <deepanshusoni613@gmail.com>
2026-05-29 16:54:42 +0530

    new changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -61,6 +61,16 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-TSTXS6L6');`,
           }}
         />
+        {/* Google Tag Manager (GTM-WSRRRM2L) */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WSRRRM2L');`,
+          }}
+        />
         {/* End Google Tag Manager */}
         <script src="https://cdn.tailwindcss.com"></script>
         <script
@@ -90,6 +100,15 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-TSTXS6L6"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* Google Tag Manager (noscript) GTM-WSRRRM2L */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WSRRRM2L"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
